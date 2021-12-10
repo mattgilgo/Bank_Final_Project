663721ee4bf7b8a276ae3e24b355a2e28d0d3b43
George Padavick <george.padavick@gmail.com>
2021-12-10 11:09:24 -0500

    Adding all files from current uml

---
+++ src/LoanTransaction.java
@@ -0,0 +1,4 @@
+package src;
+
+public class LoanTransaction extends Transaction {
+}
--- src/Manager.java
+++ src/Manager.java
@@ -1,4 +1,12 @@
 package src;
 
 public class Manager extends User {
+
+    private Report report;
+
+    public Manager() {
+
+    }
+
+
 }
