edebc9276faf43d4f874d25421ada355c89ee483
Shilpen <shilpen12@gmail.com>
2021-12-15 18:16:13 -0500

    Connecting UserLoginAndCreateUserGUI to Bank

--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,6 @@
 public class Bank {
-    
+    private ATM atm;
+    private UserLoginAndCreateUserGUI userLoginAndCreateUserGUI;
 
     public Bank(){
 
@@ -7,16 +8,31 @@ public class Bank {
 
 
     public void startBankSession(){
-
+        this.atm = new ATM();
+        this.userLoginAndCreateUserGUI = new UserLoginAndCreateUserGUI();
     }
 
+    //Create the new user account and add to database
     public void createUserAccount() {
 
     }
 
     public void userLogin(String username, String password) {
         // Check that user exists in db
-        // Determine from db if the user is a manager or customer 
+        // Determine from db if the user is a manager or customer
+
         
+    }
+
+    //getters
+
+    public ATM getAtm() {
+        return atm;
+    }
+
+    //Setters
+
+    public void setAtm(ATM atm) {
+        this.atm = atm;
     }
 }
