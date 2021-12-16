afa5b4b6aedcd8abf12689b2784400cfd5057622
Justin DiEmmanuele <justindiemmanuele@Justins-MacBook-Air.local>
2021-12-16 14:58:40 -0500

    header color

--- src/TableUICreator.java
+++ src/TableUICreator.java
@@ -13,7 +13,8 @@ public class TableUICreator extends JFrame {
         table.setModel(model);
         table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         table.setFillsViewportHeight(true);
-        table.getTableHeader().setForeground(new Color(0).white);
+        table.getTableHeader().setForeground(new Color(0).WHITE);
+        table.getTableHeader().setBackground(new Color(0).BLACK);
         JScrollPane scroll = new JScrollPane(table);
         scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
