500cdc566fda9977ff09b39f00b37b1431dc8d9c
Shilpen <shilpen12@gmail.com>
2021-12-16 15:08:43 -0500

    Fixing bug in Transaction

--- src/Transaction.java
+++ src/Transaction.java
@@ -14,6 +14,7 @@ public class Transaction { // matt removed abstract b/c it wasn't able to make a
         this.transaction_type = transaction_type;
         this.transaction_amount=transaction_amount;
         this.timestamp = new Timestamp(System.currentTimeMillis());
+        this.account_id=account_id;
     }
 
     public Transaction(int transaction_id, String transaction_type, double transaction_amount, Timestamp timestamp, int account_id) {
@@ -21,6 +22,7 @@ public class Transaction { // matt removed abstract b/c it wasn't able to make a
         this.transaction_type = transaction_type;
         this.transaction_amount=transaction_amount;
         this.timestamp = timestamp;
+        this.account_id = account_id;
     }
 
     //Getters
@@ -46,7 +48,7 @@ public class Transaction { // matt removed abstract b/c it wasn't able to make a
     }
 
     public String[] getStringArray() {
-        String [] arrayString = {Integer.toString(getTransaction_id()), getTransaction_type(), Double.toString(getTransaction_amount()), getTimestamp().toString()}; 
+        String [] arrayString = {Integer.toString(getTransaction_id()), getTransaction_type(), Double.toString(getTransaction_amount()), getTimestamp().toString(), Integer.toString(getAccount_id())};
         return arrayString;
     }
 
