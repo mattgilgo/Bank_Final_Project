05f70c5e9cf12dd318bfa3db2c2cbfdb418107ef
Justin DiEmmanuele <justindiemmanuele@Justins-MacBook-Air.local>
2021-12-15 23:43:14 -0500

    stock ui looks good first pass

---
+++ src/StockUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class StockUI {
+    private JButton seePortfolioButton;
+    private JButton seeBuysSellsButton;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JButton buyButton;
+    private JButton sellButton;
+}
