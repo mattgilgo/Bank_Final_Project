8b9abd57e3f99da340c31fc0233068e8a8528927
George Padavick <george.padavick@gmail.com>
2021-12-16 19:38:06 -0500

    getPortfolio error fix

--- src/Database.java
+++ src/Database.java
@@ -447,7 +447,7 @@ public class Database {
 
         String sql = "SELECT s.stock_id, s.stock_ticker, s.stock_price, so.stock_buy_price, SUM(so.num_shares)" +
         "FROM stocks s, stocks_owned so" +
-        "WHERE s.stock_id=so.stock_id AND so.account_id=1" +
+        "WHERE s.stock_id=so.stock_id AND so.account_id=?" +
         "GROUP BY s.stock_id";  
 
         try (
