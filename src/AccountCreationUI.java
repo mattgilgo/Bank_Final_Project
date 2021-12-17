07604a7a85c1fda948c4c8e6ac574a4a7069003f
Shilpen <shilpen12@gmail.com>
2021-12-16 19:11:11 -0500

    Added checks for numerical inputs

--- src/AccountCreationUI.java
+++ src/AccountCreationUI.java
@@ -51,7 +51,13 @@ public class AccountCreationUI extends JFrame {
                 String currency = (String) currencyDrop.getSelectedItem();
                 String acctType = (String) typeDrop.getSelectedItem();
                 String acctToCharge = (String) acctDrop.getSelectedItem();
-                Double amtToDeposit = Double.parseDouble(depositAmtField.getText());
+                Double amtToDeposit = 0.0;
+                try{
+                    amtToDeposit = Double.parseDouble(depositAmtField.getText());
+                }
+                catch (NumberFormatException ignore){
+                    System.out.println("Invalid Input!");
+                }
                 //TODO Charge the appropriate account for the balance
 
                 // create account method called here
